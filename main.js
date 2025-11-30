feb0a51a19a01ef55ff0b4d6b851e58e67911bef
kabudev <kabudev@gmail.com>
2025-11-30 01:14:40 -0300

    agregado configuración para applicación en producciion

--- main.js
+++ main.js
@@ -452,11 +452,13 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: false,
       contextIsolation: true,
       autoplayPolicy: 'no-user-gesture-required', // permite autoplay sin click
+      devTools: isDev ? true:false,
     webSecurity: true,
     allowRunningInsecureContent: false,
     plugins: true,
